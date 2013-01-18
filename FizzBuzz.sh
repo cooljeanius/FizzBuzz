bc98757d8ce0e08079ea9cb0515de31b6ef80c64
Eric Gallager <egallager@comcast.net>
2013-01-17 18:34:40 -0800

    Add a shell script version
    actually technically it's a perl one-liner that I just happened to wrap in a shell script

---
+++ FizzBuzz.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# done in a shell script instead of a perl script because I don't actually know how to write full perl scripts (yet)
+# Requires the CPAN Module "Acme::FizzBuzz" to be installed to work
+
+perl -MAcme::FizzBuzz -e ''
