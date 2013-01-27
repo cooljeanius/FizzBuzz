9e365c580e4db30824374d53ff86c46971c4f720
System Administrator <root@gl00b05044.cats.gwu.edu>
2013-01-26 20:17:15 -0500

    add test for perl

--- FizzBuzz.sh
+++ FizzBuzz.sh
@@ -2,4 +2,14 @@
 # done in a shell script instead of a perl script because I don't actually know how to write full perl scripts (yet)
 # Requires the CPAN Module "Acme::FizzBuzz" to be installed to work
 
-perl -MAcme::FizzBuzz -e ''
+echo "Testing for perl…" # Progress indicator
+PERL_PATH=`which perl`
+if [ -z "$PERL_PATH" ]; then
+	echo "Error: Perl is required to use this script."
+	exit 0
+else
+	echo "Perl found at $PERL_PATH" # Progress indicator
+fi
+
+`echo $PERL_PATH` -MAcme::FizzBuzz -e ''
+   
